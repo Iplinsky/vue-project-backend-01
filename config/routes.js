ab31058908741cea699e414bfbe0fb931907bb2e
Thiago <thiago.iplinsky19@hotmail.com>
2020-05-09 11:48:25 -0300

    CRUD Category

--- config/routes.js
+++ config/routes.js
@@ -7,4 +7,16 @@ module.exports = app => {
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
+    app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    // Chamada de rota necessária antes de /:id
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
